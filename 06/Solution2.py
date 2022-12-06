7e3db56573bb8f924e4bd4fcae1fdfe60ddb59a5
jack morrison <bobimnot123@gmail.com>
2022-12-06 10:08:02 +0000

    Day 6 solutions
    one character difference between solution 1 and 2, easy days

---
+++ 06/Solution2.py
@@ -0,0 +1,28 @@
+f = open("./File1.txt")
+arr=[]
+total=0
+for line in f:
+    buffer=[]
+    count=0
+    num=0
+    for x in line:
+        count+=1
+        
+        if(x in buffer):
+            bool=False
+            tmp=buffer.copy()
+            buffer.clear()
+            
+            for i in tmp:
+                if(bool):
+                    buffer.append(i)
+                if(i==x):
+                    bool=True
+                
+            buffer.append(x)
+        else:
+            buffer.append(x)
+        if(len(buffer)==14):
+            num=count
+            break
+    print(num)
